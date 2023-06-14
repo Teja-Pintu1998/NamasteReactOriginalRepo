0adbfa8ad597d2a2e32ee93df740d7e1064b0670
Teja-Pintu1998 <127717993+Teja-Pintu1998@users.noreply.github.com>
2023-06-14 03:29:52 -0500

    Update file.js

--- file.js
+++ file.js
@@ -1,3 +1,4 @@
 const heading = React.createElement("h1", {}, [React.createElement("h2", {}, "array 1"),React.createElement("h1",{},"array 2"),]);
+console.log(heading);
 const root  = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading)
